5077629e6ccf814422c8f9024c6467b6fa382f69
bahar-moradi <bahar.moradi33@gmail.com>
2023-03-13 09:05:02 +0100

    Fixing zipcode format

--- src/components/forms/carForm.vue
+++ src/components/forms/carForm.vue
@@ -5,7 +5,7 @@
       <p class="lead">
         Please complete the form below to start the process.
       </p>
-      <form class="form">
+      <form class="form" @submit="onSubmit($event)" novalidate>
         <div class="form-group">
           <label for="input1">LicensePlate</label>
           <input type="text" class="form-control" id="input1" placeholder="Enter license plate..." v-on:keyup="fixLisencePlateForm($event)"  @change="getCartData($event)"  pattern="[A-Z0-9]{6}" required>
@@ -19,7 +19,11 @@
          </div>
         <div class="form-group">
           <label for="input2">Zipcode</label>
-          <input type="text" class="form-control" id="input2" placeholder="0000XX">
+          <input type="text" class="form-control" id="input2" placeholder="0000XX" 
+          pattern="[1-9]{1}[0-9]{3}[A-Z]{2}" v-on:keyup=fixZipCodeFormat($event) required>
+          <div class="invalid-feedback">
+          Please provide a valide zipecode.
+        </div>
         </div>
         <div class="form-row">
           <div class="form-group col-8">
@@ -42,7 +46,7 @@
         <div class="form-group">
           <label for="input7">Kilometer Range</label>
           <select class="form-control" id="input7" v-model="selected">
-            <option v-for="option in kilometerRange" :value="option.value">{{ option.text }}</option>
+            <option v-for="kilometerRange in kilometerRanges" :value="kilometerRange.value">{{ kilometerRange.text }}</option>
           </select>
         </div>
         <button class="btn btn-primary" type="submit">Compare now</button>
@@ -64,19 +68,11 @@ import SimpleCard from './simpleCard.vue';
   },
 })
 export default class CarForm extends Vue {
-  onSubmit(): void {
+  onSubmit(e:any): void {
     console.log('Button is clicked');
   }
-
-  rdwURL: string = 'https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=';
-  carData: any[] = [{merk:"", datum_eerste_toelating: "" }];
-
-  data() {
-    return {
-
-
-      selected: '2',
-      kilometerRange: [
+      selected:string='2';
+      kilometerRanges:any= [
         { text: '0 t/m 7500 KM', value:'1'},
         { text: '7501 t/m 10000 KM', value:'2'},
         { text: '10001 t/m 12000 KM', value:'3'},
@@ -85,10 +81,12 @@ export default class CarForm extends Vue {
         { text: '20001 t/m 25000 KM', value:'6'},
         { text: '25001 t/m 30000 KM', value:'7'},
         { text: '30001 t/m 90000 KM', value:'8'},
-      ]
-    }
-  }
+        ]
 
+  rdwURL: string = 'https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=';
+  carData: any[] = [{merk:"", datum_eerste_toelating: "" }];  
+    
+  
   async getCartData(e: any): Promise<void> {
     
       console.log(`Getting data for ${e.target.value}`)
@@ -103,7 +101,14 @@ export default class CarForm extends Vue {
   fixLisencePlateForm(e:any):void{
     e.target.value=e.target.value.replace(' ','').replace('-','').toUpperCase();
   }
+
+
+  fixZipCodeFormat(e:any):void{
+    e.target.value=e.target.value.replace(' ','').replace('-','').toUpperCase();
+  }
 }
+
+
 
 
 
